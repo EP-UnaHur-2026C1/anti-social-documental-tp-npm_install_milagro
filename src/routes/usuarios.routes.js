9707a606c708855ed4f332810f19ffabef3b9e19
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-06-01 15:22:52 -0300

    feat: implementar rutas de Usuarios

---
+++ src/routes/usuarios.routes.js
@@ -0,0 +1,11 @@
+const { Router } = require('express')
+const usuariosController = require('../controllers/usuarios.controllers')
+const validarUsuario = require('../middlewares/validarUsuario')
+const validarUsuarioId = require('../middlewares/validarUsuarioId')
+const router = Router()
+
+router.get('/', usuariosController.obtenerUsuarios)
+router.get('/:id', validarUsuarioId, obtenerUsuario)
+router.post('/', validarUsuario, usuariosController.crearUsuario)
+router.put('/:id', validarUsuarioId, validarUsuario, usuariosController.editarUsuario)
+router.delete('/:id', validarUsuarioId, usuariosController.eliminarUsuario)

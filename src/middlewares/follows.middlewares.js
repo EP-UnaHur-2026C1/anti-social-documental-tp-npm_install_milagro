3929419c95f45aafbdb96a1f4b298c36bf81afa6
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-06-07 14:43:36 -0300

    bugfix: typo

--- src/middlewares/follows.middlewares.js
+++ src/middlewares/follows.middlewares.js
@@ -3,7 +3,7 @@ const { schemaFollows } = require('../schema/follows.schema');
 
 const validarFollow = (req, res, next) => {
 
-    const {error} = schemaComentarios.validate(req.body)
+    const {error} = schemaFollows.validate(req.body)
     if (error) {
         return res.status(400).json({error: `El body no cumple con los parametros solicitados: ${error.details[0].message}`})
     }
